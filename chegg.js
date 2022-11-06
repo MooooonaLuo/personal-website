1eb82649cf65160287009e293100f2855786e652
MooooonaLuo <jil728@ucsd.edu>
2022-11-06 12:13:30 -0500

    chegg full version

---
+++ chegg.js
@@ -0,0 +1,7 @@
+function onSubmit() {
+    if (document.getElementById('password').value == 'chegg530') {
+        window.location.href = 'chegg_full_version.html'; 
+       }else{ 
+           document.getElementById("alert").innerHTML = "Please try again.";
+       }
+}
