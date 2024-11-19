34cca8acc4ad53339c56a45c882d50e60980c825
MooooonaLuo <jil728@ucsd.edu>
2024-11-19 12:59:24 -0800

    add follow-up suggestion case study

---
+++ follow_up.js
@@ -0,0 +1,10 @@
+function onSubmit() {
+    if (document.getElementById('password').value == 'chegg530') {
+        window.location.href = 'follow_up_suggestion_3912.html'; 
+       }else{ 
+           document.getElementById("alert").innerHTML = "Please try again.";
+       }
+}
+
+
+
