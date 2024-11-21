2b834c58612ce34bf7248ff9f5ae73957078debc
MooooonaLuo <jil728@ucsd.edu>
2024-11-20 17:42:28 -0800

    small fix

--- chegg.js
+++ chegg.js
@@ -2,7 +2,7 @@ function onSubmit() {
     if (document.getElementById('password').value == 'chegg530') {
         window.location.href = 'chegg_full_version.html'; 
        }else{ 
-           document.getElementById("alert").innerHTML = "Please try again.";
+           document.getElementById("alert").innerHTML = "The password is incorrect. Please try again.";
        }
 }
 
--- follow_up.js
+++ follow_up.js
@@ -2,7 +2,7 @@ function onSubmit() {
     if (document.getElementById('password').value == 'chegg530') {
         window.location.href = 'follow_up_suggestion_3912.html'; 
        }else{ 
-           document.getElementById("alert").innerHTML = "Please try again.";
+           document.getElementById("alert").innerHTML = "The password is incorrect. Please try again.";
        }
 }
 
