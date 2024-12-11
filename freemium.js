d932e28bf62c773644650a3dfeee1a3e5a6999f8
MooooonaLuo <jil728@ucsd.edu>
2024-12-11 15:13:43 -0800

    added freemium

---
+++ freemium.js
@@ -0,0 +1,10 @@
+function onSubmit() {
+    if (document.getElementById('password').value == 'chegg530') {
+        window.location.href = 'freemium4829.html'; 
+       }else{ 
+           document.getElementById("alert").innerHTML = "The password is incorrect. Please try again.";
+       }
+}
+
+
+
