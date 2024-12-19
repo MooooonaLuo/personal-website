02394bc6729a90e862805037c6ab63ab3ae805a2
MooooonaLuo <jil728@ucsd.edu>
2024-12-18 23:24:06 -0800

    visual revamp on overview

--- learning_progress.js
+++ learning_progress.js
@@ -1,6 +1,6 @@
 function onSubmit() {
     if (document.getElementById('password').value == 'chegg530') {
-        window.location.href = 'follow_up_suggestion_3912.html'; 
+        window.location.href = 'learning_progress0391.html'; 
        }else{ 
            document.getElementById("alert").innerHTML = "The password is incorrect. Please try again.";
        }
